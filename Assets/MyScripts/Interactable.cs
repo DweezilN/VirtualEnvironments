c9a210dce42044a98686f87978989c31179c913e
DweezilN <nico.devonport@gmail.com>
2019-02-19 23:48:00 +0200

    Adding Sound
    Sound Added

--- Assets/MyScripts/Interactable.cs
+++ Assets/MyScripts/Interactable.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Interactable : MonoBehaviour
 {
+    AudioSource audioSource;
+
     [HideInInspector]
     public Hand m_ActiveHand = null;
+    
+    public void Update(){
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnCollisionEnter(Collision col){
+        if(col.gameObject.name == "Floor"){
+            audioSource.Play();
+        }
+    }
 }
